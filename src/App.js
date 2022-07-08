70429132ad2b9c98994e5a4569248edb9b6c4990
Soumya Banerjee <go4soumya@gmail.com>
2022-07-07 20:48:34 -0500

    chnaged greetings

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi there! Good morning.
+          Hi there! Good evening.
         </p>
         <a
           className="App-link"
